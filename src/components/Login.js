4d9799f5b7b2f68156e1286bcacec519fc46a003
denzord <45736463+denzord@users.noreply.github.com>
2021-08-10 05:47:56 +0700

    Update Login.js
    add auto complete for input email

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import { Alert, Button, Card, Form } from "react-bootstrap";
 import { useAuth } from "./../contexts/AuthContext";
-import { Link, useHistory } from 'react-router-dom'
+import { Link, useHistory } from "react-router-dom";
 
 export default function Login() {
   const emailRef = useRef();
@@ -11,20 +11,22 @@ export default function Login() {
   const [loading, setLoading] = useState(false);
   const history = useHistory();
 
-
   async function handleSubmit(e) {
     e.preventDefault();
 
     try {
       setError("");
-      setLoading(true)
+      setLoading(true);
       await login(emailRef.current.value, passwordRef.current.value);
-      history.push('/')
+      history.push("/");
     } catch (error) {
-        console.log(error.message);
-      setError("Failed to sign in");
+      console.log(error.code);
+      error.code === "auth/user-not-found"
+        ? setError("User not found")
+        : setError("Password wrong. Try again or use Forget Password");
+      // setError("Failed to sign in");
     }
-    setLoading(false)
+    setLoading(false);
   }
 
   return (
@@ -36,7 +38,7 @@ export default function Login() {
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
+              <Form.Control type="email" ref={emailRef} required autoComplete="on" name="email"/>
             </Form.Group>
             <Form.Group id="password">
               <Form.Label>Password</Form.Label>
