3dd2f9f47d2203de7f9c2909336cbdf6db2874fd
denzord <45736463+denzord@users.noreply.github.com>
2021-08-10 06:11:24 +0700

    add basename

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ function App() {
     style={{ minHeight: "100vh" }}
     >
         <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Router>
+          <Router basename={process.env.PUBLIC_URL}>
             <AuthProvider>
               <Switch>
                 <PrivateRoute exact path="/" component={Dashboard} />
